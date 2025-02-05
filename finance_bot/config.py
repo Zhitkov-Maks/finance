88a00162cb29363a3f95efd2f9c5632fe1e02fe4
maksim <m-zhitkov@inbox.ru>
2025-02-05 21:16:06 +0300

    Added urls for the bot to work.

--- finance_bot/config.py
+++ finance_bot/config.py
@@ -2,11 +2,29 @@ from typing import Dict
 
 from decouple import config
 
+
 BOT_TOKEN: str = config("TOKEN")
 API_ADDRESS: str = config("API_ADDRESS")
-BASE_URL: str = "/api/v1/"
+BASE_URL: str = API_ADDRESS + "/api/v1/"
+PAGE_SIZE = 10
 
 token_data: Dict[int, dict] = {}
-register_url: str = API_ADDRESS + BASE_URL + "auth/users/"
-login_url: str = API_ADDRESS + BASE_URL + "auth/token/login/"
-accounts_url: str = API_ADDRESS + BASE_URL + "accounts/"
+register_url: str = BASE_URL + "auth/users/"
+login_url: str = BASE_URL + "auth/token/login/"
+accounts_url: str = BASE_URL + "accounts/"
+incomes_url: str = BASE_URL + "incomes/"
+expenses_url: str = BASE_URL + "expenses/"
+
+statistic_url: dict = {
+    "statistic_expenses": expenses_url + "statistics/?month={month}&year={year}",
+    "statistic_incomes": incomes_url + "statistics/?month={month}&year={year}"
+}
+
+categories_urls: dict = {
+    "add_income": incomes_url + "category/",
+    "add_expense": expenses_url + "category/",
+    "list_expenses_category": expenses_url + "category/?page={page}&page_size={page_size}",
+    "list_incomes_category": incomes_url + "category/?page={page}&page_size={page_size}",
+    "income": incomes_url + "category/{id}/",
+    "expense": expenses_url + "category/{id}/"
+}
