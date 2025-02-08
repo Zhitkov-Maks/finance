f94649e911d58534c451ba25737908a98b25fcc9
maksim <m-zhitkov@inbox.ru>
2025-02-08 15:05:57 +0300

    Corrected the annotation

--- finance_bot/api/client.py
+++ finance_bot/api/client.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple, Dict, List
-
 import aiohttp
-
 
 @dataclass
 class Client:
@@ -16,54 +14,41 @@ class Client:
 
     async def post(self) -> Tuple[int, dict]:
         """A method for implementing post requests."""
-        async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)) as client:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as client:
             async with client.post(
-                    url=self.url, json=self.data, headers=self.header
+                url=self.url, json=self.data, headers=self.header
             ) as response:
                 data: dict = await response.json()
                 return response.status, data
 
     async def get(self) -> Tuple[int, Dict[str, List[Dict[str, int]] | Dict[str, str]]]:
         """A method for implementing get requests."""
-        async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)
-        ) as client:
-            async with client.get(
-                    url=self.url,
-                    headers=self.header
-            ) as response:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as client:
+            async with client.get(url=self.url, headers=self.header) as response:
 
-                data: Dict[str, List[Dict[str, int]] | Dict[str, str]] = await response.json()
+                data: dict = await response.json()
                 return response.status, data
 
     async def delete(self) -> Tuple[int, dict]:
         """A method for implementing delete requests."""
-        async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)
-        ) as client:
-            async with client.delete(
-                    url=self.url, headers=self.header) as response:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as client:
+            async with client.delete(url=self.url, headers=self.header) as response:
                 if response.status != 204:
                     return response.status, await response.json()
                 return response.status, {"result": True}
 
     async def patch(self) -> Tuple[int, dict]:
         """A method for implementing patch requests."""
-        async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)
-        ) as client:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as client:
             async with client.patch(
-                    url=self.url, json=self.data, headers=self.header
+                url=self.url, json=self.data, headers=self.header
             ) as response:
                 data: dict = await response.json()
                 return response.status, data
 
     async def put(self) -> Tuple[int, dict]:
         """A method for implementing put requests."""
-        async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)
-        ) as client:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as client:
             async with client.put(
                 url=self.url, json=self.data, headers=self.header
             ) as response:
