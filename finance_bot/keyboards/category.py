f0f9616814def3410506f91587f023c7019ece40
maksim <m-zhitkov@inbox.ru>
2025-02-08 14:28:38 +0300

    Implemented keyboards for working with categories.

---
+++ finance_bot/keyboards/category.py
@@ -0,0 +1,92 @@
+from typing import List
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def get_categories_action(back: str) -> InlineKeyboardMarkup:
+    """
+    The function generates a keyboard for working with categories.
+    :param back: A string that will indicate which action
+                    to assign to the back button.
+    :return: A keyboard for working with categories.
+    """
+    actions_buttons: list[list[InlineKeyboardButton]] = [
+        [
+            InlineKeyboardButton(text="Удалить", callback_data="del_category"),
+            InlineKeyboardButton(text="Меню", callback_data="main"),
+            InlineKeyboardButton(text="Ред-ть", callback_data="edit_category"),
+        ],
+        [InlineKeyboardButton(text="Назад", callback_data=f"{back}")],
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=actions_buttons)
+
+
+async def create_list_category(
+    data: dict[str, int | list[dict[str, int | str]]],
+    prev: str = "prev_cat_inc",
+    next_cat: str = "next_cat_inc",
+) -> InlineKeyboardMarkup:
+    """
+    Creates an inline keyboard.
+    :param next_cat: The name for the callback_data when
+                        forming the keyboard.
+    :param prev: The name for the callback_data when
+                    forming the keyboard.
+    :param data: Dictionary with query data.
+    :return: The inline keyboard.
+    """
+    inline_buttons: List[List[InlineKeyboardButton]] = []
+    previous, next_ = data.get("previous"), data.get("next")
+    for item in data.get("results"):
+        inline_buttons.append(
+            [
+                InlineKeyboardButton(
+                    text=f"{item.get("name")}", callback_data=str(item.get("id"))
+                )
+            ]
+        )
+
+    prev_data, text_prev = "None prev", "-"
+    next_data, text_next = "None next", "-"
+
+    if previous is not None:
+        prev_data, text_prev = prev, "<<"
+
+    if next_ is not None:
+        next_data, text_next = next_cat, ">>"
+
+    inline_buttons.append(
+        [
+            InlineKeyboardButton(text=text_prev, callback_data=prev_data),
+            InlineKeyboardButton(text="Меню", callback_data="main"),
+            InlineKeyboardButton(text=text_next, callback_data=next_data),
+        ]
+    )
+    inline_buttons.append(
+        [
+            InlineKeyboardButton(
+                text="Меню категорий",
+                callback_data="categories",
+            )
+        ]
+    )
+    return InlineKeyboardMarkup(inline_keyboard=inline_buttons)
+
+
+type_category_buttons: list[list[InlineKeyboardButton]] = [
+    [
+        InlineKeyboardButton(text="Доходы ➕ кат.", callback_data="add_income"),
+        InlineKeyboardButton(text="Расходы ➕ кат.", callback_data="add_expense"),
+    ],
+    [
+        InlineKeyboardButton(text="Доходы", callback_data="list_incomes_category"),
+        InlineKeyboardButton(text="В меню", callback_data="main"),
+        InlineKeyboardButton(text="Расходы", callback_data="list_expenses_category"),
+    ],
+]
+
+# The keyboard is used to select an action when clicking on the Work with categories button.
+inline_type_categories: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=type_category_buttons
+)
