6f0fb37211d14d0261fd925c2c930bfc52c1d83f
maksim <m-zhitkov@inbox.ru>
2025-02-15 16:32:11 +0300

    Added the ability to reset the password.

--- finance/app_user/views.py
+++ finance/app_user/views.py
@@ -1,1 +1,17 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from djoser.email import PasswordResetEmail
 
+
+class CustomPasswordResetEmail(PasswordResetEmail):
+    """
+    A class for adding the necessary context and replacing the standard message.
+    """
+
+    def get_context_data(self) -> dict:
+        """Adding the necessary data to the context."""
+        context = super().get_context_data()
+        user = context.get("user")
+        context["uid"] = context.get("uid")
+        context["token"] = PasswordResetTokenGenerator()
+        context["email"] = user.email if user else ""
+        return context
