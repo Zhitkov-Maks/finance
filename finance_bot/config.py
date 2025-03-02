cc3d9ef39f1dda8ffad8ffc122ed4bb4edc8d9a1
maksim <m-zhitkov@inbox.ru>
2025-03-02 15:52:06 +0300

    Added a url for dealing with debts.

--- finance_bot/config.py
+++ finance_bot/config.py
@@ -6,20 +6,30 @@ from decouple import config
 BOT_TOKEN: str = config("TOKEN")
 API_ADDRESS: str = config("API_ADDRESS")
 BASE_URL: str = API_ADDRESS + "/api/v1/"
-PAGE_SIZE = 1
+PAGE_SIZE = 5
 
 token_data: Dict[int, dict] = {}
+
+# -------------------AUTH----------------------------------------------------
+
 register_url: str = BASE_URL + "auth/users/"
 login_url: str = BASE_URL + "auth/token/login/"
+
+# ----------------EXPENSES INCOMES ACCOUNTS----------------------------------
+
 accounts_url: str = BASE_URL + "accounts/"
 incomes_url: str = BASE_URL + "incomes/"
 expenses_url: str = BASE_URL + "expenses/"
+
+# ------------------STATISTICS-----------------------------------------------
 
 statistic_url: dict = {
     "statistic_expenses": expenses_url +
                           "statistics/?month={month}&year={year}",
     "statistic_incomes": incomes_url + "statistics/?month={month}&year={year}"
 }
+
+# -------------------CATEGORIES-----------------------------------------------
 
 categories_urls: dict = {
     "add_income": incomes_url + "category/",
@@ -32,8 +42,22 @@ categories_urls: dict = {
     "expense": expenses_url + "category/{id}/"
 }
 
+# -------------------RESET PASSWORD-------------------------------------------
+
 reset_password_url = BASE_URL + "auth/users/reset_password/"
 reset_password_confirm_url = BASE_URL + "auth/users/reset_password_confirm/"
 
-debts_url: str = (BASE_URL +
-                  "accounts/debts/?type={type}&page={page}&page_size={page_size}")
+
+# -------------------DEBT-----------------------------------------------------
+debts_url: str = (BASE_URL + "accounts/debts/"
+                             "?type={type}"
+                             "&page={page}"
+                             "&page_size={page_size}"
+                  )
+
+debts_url_by_id: str = (BASE_URL + "accounts/debts/{debt_id}/")
+debts_repay_url: str = (BASE_URL + "accounts/debts/repay-debt/")
+debt_create_url: str = (BASE_URL + "accounts/debts/create-debt/")
+debt_create_accounts_url: str = (
+        BASE_URL + "accounts/debts/create-debt-accounts/"
+)
