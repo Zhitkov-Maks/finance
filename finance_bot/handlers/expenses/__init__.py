5395df2109ded4c4d57cd196b3a7db0298d02c1f
Maksim <m-zhitkov@inbox.ru>
2025-09-05 00:06:17 +0300

    I transferred the duplicate code with expenses and income in the transaction.

--- finance_bot/handlers/expenses/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "expense_router",
-    "exp_edit_router",
-    "create_exp_router"
-]
-
-from handlers.expenses.create_expense import create_exp_router
-from handlers.expenses.edit_expense import exp_edit_router
-from handlers.expenses.expenses import expense_router
--- finance_bot/handlers/incomes/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [
-    "incomes",
-    "create_inc_router"
-]
-
-from handlers.incomes.create_income import create_inc_router
-from handlers.incomes.income import incomes
