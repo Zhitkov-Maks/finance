b86bfbeab678f9e6437d5d731e730be21e382d90
Maksim <m-zhitkov@inbox.ru>
2026-05-07 11:45:00 +0300

    Fixes for proper operation.

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # os.getenv("DEBUG", "False")
+DEBUG = os.getenv("DEBUG", "False")
 
 # Домены для продакшена
 ALLOWED_HOSTS = [
